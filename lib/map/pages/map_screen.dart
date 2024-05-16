d8e09bd06d292a72b1d30f073f072e4805051983
ToshevaDurdona <toshevadurdona2002@gmail.com>
2024-05-16 12:39:54 +0500

    map

--- lib/map/pages/map_screen.dart
+++ lib/map/pages/map_screen.dart
@@ -1,39 +1,74 @@
+
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:yandex_mapkit/yandex_mapkit.dart';
 import '../model/app_lat_long.dart';
 import '../service/location_service.dart';
 
-class MapScreen extends StatefulWidget {
-  const MapScreen({Key? key}) : super(key: key);
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
 
   @override
-  State<MapScreen> createState() => _MapScreenState();
+  State<MapPage> createState() => _MapPageState();
 }
 
-class _MapScreenState extends State<MapScreen> {
+class _MapPageState extends State<MapPage> {
   final mapControllerCompleter = Completer<YandexMapController>();
+  String addressDetail = "Map Page";
 
   @override
   void initState() {
     super.initState();
     _initPermission().ignore();
+    // Token Api -> save access va refresh token
+    // get access -> distance Api
+
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Joriy joylashuv'),
+        toolbarHeight: 2,
       ),
-      body: YandexMap(
-        onMapCreated: (controller) {
-          mapControllerCompleter.complete(controller);
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _fetchCurrentLocation();
         },
+        backgroundColor: Colors.white,
+        child: const Icon(Icons.data_saver_on),
+      ),
+
+      body: Stack(
+        children: [
+          YandexMap(
+            onMapCreated: (controller) {
+              mapControllerCompleter.complete(controller);
+            },
+            onCameraPositionChanged: (cameraPosition, reason, finished) {
+              if (finished) {
+                updateAddressDetail(AppLatLong(
+                    lat: cameraPosition.target.latitude,
+                    long: cameraPosition.target.longitude));
+              }
+            },
+          ),
+          const Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: 0,
+            child: Icon(
+              Icons.location_on,
+              size: 40,
+              color: Colors.red,
+            ),
+          ),
+
+        ],
       ),
     );
   }
-
   /// Foydalanuvchining joylashuviga kirish uchun ruxsatlar tekshirilmoqda
   Future<void> _initPermission() async {
     if (!await LocationService().checkPermission()) {
@@ -41,7 +76,6 @@ class _MapScreenState extends State<MapScreen> {
     }
     await _fetchCurrentLocation();
   }
-
   /// Foydalanuvchining joriy geolokatsiyasini olish
   Future<void> _fetchCurrentLocation() async {
     AppLatLong location;
@@ -51,13 +85,14 @@ class _MapScreenState extends State<MapScreen> {
     } catch (_) {
       location = defLocation;
     }
+    updateAddressDetail(location);
     _moveToCurrentLocation(location);
   }
 
   /// Joriy pozitsiyani ko'rsatish usuli
   Future<void> _moveToCurrentLocation(
-    AppLatLong appLatLong,
-  ) async {
+      AppLatLong appLatLong,
+      ) async {
     (await mapControllerCompleter.future).moveCamera(
       animation: const MapAnimation(type: MapAnimationType.linear, duration: 1),
       CameraUpdate.newCameraPosition(
@@ -66,9 +101,24 @@ class _MapScreenState extends State<MapScreen> {
             latitude: appLatLong.lat,
             longitude: appLatLong.long,
           ),
-          zoom: 12,
+          zoom: 13,
         ),
       ),
     );
   }
+
+  Future<void> updateAddressDetail(AppLatLong latLong) async {
+    // addressDetail = "...loading";
+    // setState(() {});
+    // AddressDetailModel  data = await repository.getAddressDetail(latLong);
+    // addressDetail = data!.responset!.geoObjectCollection!.featureMember!.isEmpty
+    //     ? "unknown_place"
+    //     : data.responset!.geoObjectCollection!.featureMember![0].geoObject!
+    //     .metaDataProperty!.geocoderMetaData!.address!.formatted
+    //     .toString();
+    // setState(() {});
+    // print(addressDetail);
+  }
 }
+
+
