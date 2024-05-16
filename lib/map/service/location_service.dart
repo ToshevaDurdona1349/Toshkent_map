a1c1beccac5251dbd91061f96c202372cb3ea83f
ToshevaDurdona <toshevadurdona2002@gmail.com>
2024-05-16 16:08:23 +0500

    service http add prefs

--- lib/map/service/location_service.dart
+++ lib/map/service/location_service.dart
@@ -1,5 +1,7 @@
 import 'package:geolocator/geolocator.dart';
 import '../model/app_lat_long.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 abstract class AppLocation {
   Future<AppLatLong> getCurrentLocation();
@@ -37,5 +39,33 @@ class LocationService implements AppLocation {
             value == LocationPermission.always ||
             value == LocationPermission.whileInUse)
         .catchError((_) => false);
+  }
+
+
+  Future<void> getDistance() async {
+    final apiKey = 'Aa7274830.123456789-0123456789_0';
+    final origin = 'Nurhayot';
+    final destination = 'Los Angeles, CA';
+    final mode = 'driving';
+    final message = 5731.892253110097;
+    final status =200;// or 'walking', 'bicycling', etc.
+
+    final url = 'https://api.example.com/distance?'
+        'origin=$origin&'
+        'destination=$destination&'
+        'mode=$mode&'
+        'message=$message&'
+        'status=$status&'
+        'key=$apiKey';
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final distanceData = jsonDecode(response.body);
+      final distance = distanceData['distance']; // Extract distance from response
+      print('Distance: $distance');
+    } else {
+      print('Failed to get distance: ${response.statusCode}');
+    }
   }
 }
